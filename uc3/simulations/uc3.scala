19f6f66c4fe2642a86e2073371676f9e8418a9c7
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2020-03-25 13:43:20 +0100

    Add api echo-request (#1)

--- uc3/simulations/uc3.scala
+++ uc3/simulations/uc3.scala
@@ -1,9 +1,3 @@
-//********************************************************************
-//* CHANGELOG
-//*
-//* 03/05/2019 - Jason Rizio - Updated example for Gatling 3.0
-//********************************************************************
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
@@ -12,59 +6,40 @@ import scala.concurrent.duration._
 import java.util.Date._
 import java.text.SimpleDateFormat._
 
-// We upload this simulation separately to the user-files.zip
-// in this advanced example so that Flood IO can parse the
-// simulation name to execute.
 class uc3 extends Simulation {
 
   val baseUrl                = System.getProperty("baseUrl")
-  val token                  = System.getProperty("token")
+  val apikeyHeaderKey        = System.getProperty("apikey-header-key")
+  val apikeyHeaderValue      = System.getProperty("apikey-header-value")
 
   val maxHostConcurrentUsers = Integer.getInteger("maxHostConcurrentUsers", 10)
   val incrementFactor        = maxHostConcurrentUsers.toFloat/10
   
   val httpConf = http
     .baseUrl(baseUrl)
-    .authorizationHeader(s"Bearer $token")
-    .acceptHeader("application/json, */*")
-    .acceptCharsetHeader("UTF-8")
+    .header(s"${apikeyHeaderKey}", s"${apikeyHeaderValue}")
+    .header("Content-Type", "application/json")
 
-  object OpenIOApp {
-    val openIOApp = 
+  object EchoRequest {
+
+    val echoRequest = 
       exec(
-        http("Get user's profile")
-          .get("/api/v1/profile")
-          .check(status.is(200))
+        http("Echo Request")
+          .get("/test/echo-request")
+          .check(jsonPath("$.method").is("GET"))
       )
-      .exec(
-        http("Get user's messages")
-          .get("/api/v1/messages")
-          .check(status.is(200))
-          .check(jsonPath("$.items..id").findAll.saveAs("messageIds"))
-      )
-      .foreach("${messageIds}", "messageId") {
-        exec(
-          http("Get message")
-          .get("/api/v1/messages/${messageId}")
-          check(status.is(200))
-        )
-      }
-      .exec(
-        http("Get all visible services")
-          .get("/api/v1/services")
-          .check(status.is(200))
-      )
-      .pause(1000 milliseconds, 3000 milliseconds)
-  }
 
-  val users = scenario("Users").exec(OpenIOApp.openIOApp)
+  }
+  
+  val users = scenario("Users").exec(EchoRequest.echoRequest)
 
   setUp(
     users.inject(
-      incrementConcurrentUsers(incrementFactor.toInt) // Double
-        .times(10) // repetition
-        .eachLevelLasting(30 seconds) // time between next increment
-        .startingFrom(incrementFactor.toInt) // Double
+      incrementUsersPerSec(incrementFactor.toInt)
+        .times(5)
+        .eachLevelLasting(30 seconds)
+        .separatedByRampsLasting(10 seconds)
+        .startingFrom(incrementFactor.toInt)
     ).protocols(httpConf)
   )
 
