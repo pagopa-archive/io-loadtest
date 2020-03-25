46937508c3db0b5859373732c2e48770d2ada586
pasqualedevita <{ID}+{username}@users.noreply.github.com>
2020-03-25 13:11:48 +0100

    stress test vars

--- uc2/simulations/uc2.scala
+++ uc2/simulations/uc2.scala
@@ -1,9 +1,3 @@
-//********************************************************************
-//* CHANGELOG
-//*
-//* 03/05/2019 - Jason Rizio - Updated example for Gatling 3.0
-//********************************************************************
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
@@ -12,9 +6,6 @@ import scala.concurrent.duration._
 import java.util.Date._
 import java.text.SimpleDateFormat._
 
-// We upload this simulation separately to the user-files.zip
-// in this advanced example so that Flood IO can parse the
-// simulation name to execute.
 class uc2 extends Simulation {
 
   val baseUrl                = System.getProperty("baseUrl")
@@ -33,17 +24,18 @@ class uc2 extends Simulation {
           .get("/ping")
           .check(status.is(200))
       )
-      .pause(1000 milliseconds, 3000 milliseconds)
+      .pause(1000 milliseconds)
   }
 
   val users = scenario("Users").exec(Ping.ping)
 
   setUp(
     users.inject(
-      incrementConcurrentUsers(incrementFactor.toInt) // Double
-        .times(10) // repetition
-        .eachLevelLasting(30 seconds) // time between next increment
-        .startingFrom(incrementFactor.toInt) // Double
+      incrementUsersPerSec(incrementFactor.toInt)
+        .times(5)
+        .eachLevelLasting(30 seconds)
+        .separatedByRampsLasting(10 seconds)
+        .startingFrom(incrementFactor.toInt)
     ).protocols(httpConf)
   )
 
