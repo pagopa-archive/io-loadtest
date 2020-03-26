dc2b82a564e2013e59c253c8a5ed4c9acbb87ee7
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2020-03-26 11:50:19 +0100

    [#171995876] Add app-backend getProfile (#3)

---
+++ uc4/simulations/uc4.scala
@@ -0,0 +1,66 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+import java.util.Date._
+import java.text.SimpleDateFormat._
+
+class uc4 extends Simulation {
+
+  val baseUrl                = System.getProperty("baseUrl")
+  val sessionToken           = System.getProperty("sessionToken")
+
+  val model                       = System.getProperty("model")
+  val steps                       = Integer.getInteger("steps", 5)
+  val maxHostConcurrentUsers      = Integer.getInteger("maxHostConcurrentUsers", 10)
+  val maxHostIncrementUsersPerSec = Integer.getInteger("maxHostIncrementUsersPerSec", 10)
+  
+  val httpConf = http
+    .baseUrl(baseUrl)
+    .authorizationHeader(s"Bearer $sessionToken")
+    .header("Content-Type", "application/json")
+
+  object GetProfile {
+
+    val getProfile = 
+      exec(
+        http("Get Profile")
+          .get("/api/v1/profile")
+      )
+
+  }
+  
+  val users = scenario("Users").exec(GetProfile.getProfile)
+
+  model match {
+
+    case "open"  => 
+    // Open Model
+    val incrementUsers = maxHostIncrementUsersPerSec.toFloat/steps
+    setUp(
+      users.inject(
+        incrementUsersPerSec(incrementUsers.toInt) // Double
+          .times(steps.toInt) // repetition
+          .eachLevelLasting(30 seconds) // time between next increment
+          .separatedByRampsLasting(10 seconds)
+          .startingFrom(incrementUsers.toInt) // Double
+      ).protocols(httpConf)
+    )
+
+    case "closed"  => 
+    // Closed Model
+    val incrementUsers = maxHostConcurrentUsers.toFloat/steps
+    setUp(
+      users.inject(
+        incrementConcurrentUsers(incrementUsers.toInt) // Double
+          .times(steps.toInt) // repetition
+          .eachLevelLasting(30 seconds) // time between next increment
+          .separatedByRampsLasting(10 seconds)
+          .startingFrom(incrementUsers.toInt) // Double
+      ).protocols(httpConf)
+    )
+
+  }
+
+}
