037977f6c7fe3f733ea7dc3cb718cc54bdc61761
pasqualedevita <{ID}+{username}@users.noreply.github.com>
2020-03-26 18:13:24 +0100

    Add uc6 app-backend getMessage

---
+++ uc6/simulations/uc6.scala
@@ -0,0 +1,69 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+import java.util.Date._
+import java.text.SimpleDateFormat._
+
+class uc6 extends Simulation {
+
+  val baseUrl                = System.getProperty("baseUrl")
+  val sessionToken           = System.getProperty("sessionToken")
+  val messagesFile           = System.getProperty("messagesFile")
+
+  val model                       = System.getProperty("model")
+  val steps                       = Integer.getInteger("steps", 5)
+  val maxHostConcurrentUsers      = Integer.getInteger("maxHostConcurrentUsers", 10)
+  val maxHostIncrementUsersPerSec = Integer.getInteger("maxHostIncrementUsersPerSec", 10)
+
+  val messagesFeeders = csv(s"$messagesFile").eager.random
+
+  val httpConf = http
+    .baseUrl(baseUrl)
+    .authorizationHeader(s"Bearer $sessionToken")
+    .header("Content-Type", "application/json")
+
+  object GetMessage {
+
+    val getMessage = 
+      exec(
+        http("Get Message")
+          .get("/api/v1/messages/${messageId}")
+      )
+
+  }
+  
+  val users = scenario("Users").feed(messagesFeeders).exec(GetMessage.getMessage)
+
+  model match {
+
+    case "open"  => 
+    // Open Model
+    val incrementUsers = maxHostIncrementUsersPerSec.toFloat/steps
+    setUp(
+      users.inject(
+        incrementUsersPerSec(incrementUsers.toInt) // Double
+          .times(steps.toInt) // repetition
+          .eachLevelLasting(30 seconds) // time between next increment
+          .separatedByRampsLasting(10 seconds)
+          .startingFrom(incrementUsers.toInt) // Double
+      ).protocols(httpConf)
+    )
+
+    case "closed"  => 
+    // Closed Model
+    val incrementUsers = maxHostConcurrentUsers.toFloat/steps
+    setUp(
+      users.inject(
+        incrementConcurrentUsers(incrementUsers.toInt) // Double
+          .times(steps.toInt) // repetition
+          .eachLevelLasting(30 seconds) // time between next increment
+          .separatedByRampsLasting(10 seconds)
+          .startingFrom(incrementUsers.toInt) // Double
+      ).protocols(httpConf)
+    )
+
+  }
+
+}
