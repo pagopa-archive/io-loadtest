03b3c8fc93980c32df5511eb0b0c61695ee90ef7
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2020-03-26 18:15:28 +0100

    [#172003734] Add uc6 app-backend getMessage (#5)

---
+++ uc6/vars-300rps.sh
@@ -0,0 +1,19 @@
+## rename this file in vars.sh
+
+#Change to your simulation path 
+GATLING_WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#Change to your simulation class name
+SIMULATION_NAME="uc6"
+
+JAVA_OPTS="${JAVA_OPTS} -DbaseUrl=https://app-backend.io.italia.it"
+
+JAVA_OPTS="${JAVA_OPTS} -DsessionToken=XXX"
+JAVA_OPTS="${JAVA_OPTS} -DmessagesFile=messages.csv"
+
+JAVA_OPTS="${JAVA_OPTS} -Dmodel=closed"
+JAVA_OPTS="${JAVA_OPTS} -Dsteps=5"
+# for closed model
+JAVA_OPTS="${JAVA_OPTS} -DmaxHostConcurrentUsers=300"
+# for open model
+JAVA_OPTS="${JAVA_OPTS} -DmaxHostIncrementUsersPerSec=10"
