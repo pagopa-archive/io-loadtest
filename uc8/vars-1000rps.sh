c8e7ae4512cc40aad943aae6be91ab1b8973adef
pasqualedevita <{ID}+{username}@users.noreply.github.com>
2020-03-30 16:24:23 +0200

    Add uc8 api postMessage

---
+++ uc8/vars-1000rps.sh
@@ -0,0 +1,21 @@
+## rename this file in vars.sh
+
+#Change to your simulation path 
+GATLING_WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#Change to your simulation class name
+SIMULATION_NAME="uc8"
+
+JAVA_OPTS="${JAVA_OPTS} -DbaseUrl=https://api.io.italia.it"
+
+JAVA_OPTS="${JAVA_OPTS} -Dapikey-header-key=Ocp-Apim-Subscription-Key"
+JAVA_OPTS="${JAVA_OPTS} -Dapikey-header-value=XXX"
+JAVA_OPTS="${JAVA_OPTS} -DfiscalcodesFile=fiscalcodes.csv"
+JAVA_OPTS="${JAVA_OPTS} -DmaxMessageTemplateId=3"
+
+JAVA_OPTS="${JAVA_OPTS} -Dmodel=open"
+JAVA_OPTS="${JAVA_OPTS} -Dsteps=5"
+# for closed model
+JAVA_OPTS="${JAVA_OPTS} -DmaxHostConcurrentUsers=1000"
+# for open model
+JAVA_OPTS="${JAVA_OPTS} -DmaxHostIncrementUsersPerSec=1000"
