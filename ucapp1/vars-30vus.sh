9a074d4aec8ff53af8375def1c45482540b7e201
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2020-04-01 16:23:52 +0200

    [#172101309] Add ucapp1 OpenAppIO and ReadMessages

---
+++ ucapp1/vars-30vus.sh
@@ -0,0 +1,19 @@
+## rename this file in vars.sh
+
+#Change to your simulation path 
+GATLING_WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#Change to your simulation class name
+SIMULATION_NAME="ucapp1"
+
+JAVA_OPTS="${JAVA_OPTS} -DbaseUrl=https://app-backend.io.italia.it"
+JAVA_OPTS="${JAVA_OPTS} -DsessionToken=XXX"
+JAVA_OPTS="${JAVA_OPTS} -DmessageReadCountDistributionFile=messageReadCountDistribution.csv"
+
+JAVA_OPTS="${JAVA_OPTS} -Dmodel=closed"
+JAVA_OPTS="${JAVA_OPTS} -DsteadyStateTime=600"
+JAVA_OPTS="${JAVA_OPTS} -Dsteps=5"
+# for closed model
+JAVA_OPTS="${JAVA_OPTS} -DmaxHostConcurrentUsers=30"
+# for open model
+JAVA_OPTS="${JAVA_OPTS} -DmaxHostIncrementUsersPerSec=30"
